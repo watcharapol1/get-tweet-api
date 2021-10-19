a13d367f2c3639b0307ff8ddbb6c08d639f27a84
watcharapol1 <91687237+watcharapol1@users.noreply.github.com>
2021-10-19 16:54:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
 import pandas as pd
-# import pyodbc 
 import tweepy as tw
 import json
 
-#############################################################################################
-################################  DB SETUP  #################################################
-
-# server = '192.168.75.126' 
-# database = 'DB_OpenData' 
-# username = 'sa' 
-# password = 'Bj4free' 
-# conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-# cursor = conn.cursor()
 
 #############################################################################################
 ################################  TWEEPY SETUP  #############################################
@@ -42,16 +32,12 @@ def search_tweets():
     
     # To dateframe
     tweet_df = pd.DataFrame(data=users_locs, columns=['time_stamp', 'text', 'followers_count', 'retweet_count','favorite_count'])
-
-    # for index, row in tweet_df.iterrows():
-    #     cursor.execute("INSERT INTO dbo.data_tweet (date_time,tweet_text,retweet) values(?,?,?)", row.time_stamp, row.text, row.retweet_count)
-    #     conn.commit()
-
+    
     # To Json  
-    result = tweet_df['text'].to_json(orient="index")
+    result = tweet_df['text'].to_json(orient="records")
     parsed = json.loads(result)
 
-    return parsed
+    return jsonify(parsed)
 
 
 @app.route('/', methods=['GET'])
@@ -59,7 +45,7 @@ def home():
     return 'Hello World'
 
 @app.route('/api/get-tweet', methods=['GET'])
-def get_api(keyword):
+def get_api():
     return search_tweets()
 
 if __name__ == "__main__":
